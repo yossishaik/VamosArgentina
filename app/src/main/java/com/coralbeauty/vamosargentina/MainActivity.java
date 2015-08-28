75bbf4ac9f186c9c1b7406bbea482363d0316c23
yossishaik <yossishaik@gmail.com>
2015-08-28 19:30:26 +0300

    vamoss

--- app/src/main/java/com/coralbeauty/vamosargentina/MainActivity.java
+++ app/src/main/java/com/coralbeauty/vamosargentina/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(MainActivity.this,"VAMOS ARGENTINA",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"VAMOד ARGENTINA",Toast.LENGTH_LONG).show();
             }
         });
     }
